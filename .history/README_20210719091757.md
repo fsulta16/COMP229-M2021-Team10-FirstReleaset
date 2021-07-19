ffefa794181dec3605590fd18d88d3bf7e3a1528
fsulta16 <fsulta16@my.centennialcollege.ca>
2021-07-19 09:30:55 -0400

    updated comments and testing was initiated

---
+++ .history/README_20210719091757.md
@@ -0,0 +1,5 @@
+# COMP229-M2021-Team10-First Release
+
+## Welcome to the First Release Project - QFHO Tourney Bracket Site
+
+please use **`npm install`** to install project dependencies
---
+++ Client/Scripts/app.ts
@@ -0,0 +1,36 @@
+/*
+  Module dependencies.
+   File name: app.ts, 
+    Author's name: Ofovwe Ewere
+    Student's id: 301188196
+    Web App name: Tournament list
+    Date: July 18, 2021
+ */
+//IIFE -- Immediately Invoked Function Expression
+"use strict";
+
+(function(){
+
+    function confirmDelete()
+    {
+      // confirm deletion
+      $("a.delete").on("click", function(event){
+        if(!confirm("Are you sure?"))
+        {
+          event.preventDefault();
+          location.href = '/clothing-list';
+        }       
+      });
+    }
+
+    function Start():void
+    {
+        console.log("App Started");
+        
+        confirmDelete();  
+    }
+
+    window.addEventListener("load", Start);
+
+})();
+
